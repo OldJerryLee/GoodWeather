aa3649038efa37c23dcfffd120290a444d3df751
Fabricio Pujol <Fabricio_2112@hotmail.com>
2022-05-07 17:49:20 -0300

    AddWeather Screen

--- GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class AddWeatherCityViewController: UIViewController {
 
+    @IBOutlet var cityNameTextField: UITextField!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+
+    @IBAction func close(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func saveCityButtonPressed(_ sender: Any) {
+    }
+
 }
